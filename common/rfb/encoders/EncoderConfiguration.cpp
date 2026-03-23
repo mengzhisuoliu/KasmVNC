4a1af0e57f148459638789d2beae1c6b3813b1ce
El <eldargafiyatov@gmail.com>
2026-03-23 02:03:55 +0000

    VNC-324 Update allowed quality ranges for AV1, H.264, and H.265 encoders in EncoderConfiguration

--- common/rfb/encoders/EncoderConfiguration.cpp
+++ common/rfb/encoders/EncoderConfiguration.cpp
@@ -1,5 +1,5 @@
 /* Copyright (C) 2025 Kasm.  All Rights Reserved.
-*
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -19,41 +19,45 @@
 
 namespace rfb {
     EncoderConfiguration::Range h264_quality_range = {0, 51};
+    EncoderConfiguration::Range h264_allowed_range = {0, 50};
     EncoderConfiguration::Range av1_quality_range = {0, 63};
-    static inline std::array<EncoderConfiguration, static_cast<size_t>(KasmVideoEncoders::Encoder::unavailable) + 1> EncoderConfigurations =
-        {
+    EncoderConfiguration::Range av1_allowed_range = {0, 62};
+
+    static inline std::array<EncoderConfiguration, static_cast<size_t>(KasmVideoEncoders::Encoder::unavailable) + 1>
+        EncoderConfigurations = {
             // AV1
             // av1_vaapi
-            EncoderConfiguration{av1_quality_range, av1_quality_range, {}},
+            EncoderConfiguration{av1_quality_range, av1_allowed_range, {}},
             // av1_ffmpeg_vaapi
-            EncoderConfiguration{av1_quality_range, av1_quality_range, {}},
+            EncoderConfiguration{av1_quality_range, av1_allowed_range, {}, 0},
             // av1_nvenc
-            EncoderConfiguration{av1_quality_range, av1_quality_range, {18, 23, 28, 39, 63}},
+            EncoderConfiguration{av1_quality_range, av1_allowed_range, {18, 23, 28, 39, 62}, 0},
             // av1_software
             // profile AV_PROFILE_AV1_MAIN/FF_PROFILE_AV1_MAIN = 0
-            EncoderConfiguration{av1_quality_range, av1_quality_range, {18, 23, 28, 39, 63}, 0},
+            EncoderConfiguration{av1_quality_range, av1_allowed_range, {18, 23, 28, 39, 62}, 0},
 
             // H.265
             // h265_vaapi
             EncoderConfiguration{h264_quality_range, h264_quality_range, {18, 23, 28, 39, 51}},
             // h265_ffmpeg_vaapi
-            EncoderConfiguration{h264_quality_range, h264_quality_range, {18, 23, 28, 39, 51}},
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {18, 23, 28, 39, 50}, 1},
             // h265_nvenc
-            EncoderConfiguration{h264_quality_range, h264_quality_range, {18, 23, 28, 39, 51}},
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {18, 23, 28, 39, 50}, 1},
             // h265_software
             // profile AV_PROFILE_HEVC_MAIN/FF_PROFILE_HEVC_MAIN = 1
-            EncoderConfiguration{h264_quality_range, {0, 50}, {18, 23, 28, 39, 50}, 1},
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {18, 23, 28, 39, 50}, 1},
 
             // H.264
             // h264_vaapi
             EncoderConfiguration{h264_quality_range, h264_quality_range, {18, 23, 28, 33, 51}},
             // h264_ffmpeg_vaapi
-            EncoderConfiguration{h264_quality_range, h264_quality_range, {12, 16, 25, 39, 51}},
+            // profile  AV_PROFILE_H264_MAIN = 77
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {12, 16, 25, 39, 50}, 77},
             // h264_nvenc
-            EncoderConfiguration{h264_quality_range, h264_quality_range, {18, 23, 28, 39, 51}},
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {18, 23, 28, 39, 50}, 77},
             // h264_software
             // profile AV_PROFILE_H264_CONSTRAINED_BASELINE/AV_PROFILE_H264_CONSTRAINED_BASELINE = (66|(1<<9))
-            EncoderConfiguration{h264_quality_range, {0, 50}, {9, 18, 25, 39, 50}, (66|1<<9)},
+            EncoderConfiguration{h264_quality_range, h264_allowed_range, {9, 18, 25, 39, 50}, (66 | 1 << 9)},
 
             EncoderConfiguration{}
     };
