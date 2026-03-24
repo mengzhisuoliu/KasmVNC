b8a9889467fe0c3f75576a993d2d4cc68c089a8e
El <eldargafiyatov@gmail.com>
2026-03-24 13:29:01 +0000

    VNC-333 Remove h.265 SW from KasmVNC

--- common/rfb/encoders/KasmVideoEncoders.h
+++ common/rfb/encoders/KasmVideoEncoders.h
@@ -87,7 +87,6 @@ namespace rfb {
             h265_vaapi, // h265
             h265_ffmpeg_vaapi,
             h265_nvenc,
-            h265_software,
 
             h264_vaapi,
             h264_ffmpeg_vaapi,
@@ -112,8 +111,7 @@ namespace rfb {
 
             "hevc_vaapi",
             "hevc_vaapi",
-            "hevc_nvenc",
-            "libx265",
+            "hevc_nvenc", // "libx265"
 
             "h264_vaapi",
             "h264_vaapi",
@@ -131,7 +129,6 @@ namespace rfb {
             pseudoEncodingStreamingModeHEVCVAAPI,
             pseudoEncodingStreamingModeHEVCVAAPI,
             pseudoEncodingStreamingModeHEVCNVENC,
-            pseudoEncodingStreamingModeHEVCSW,
 
             pseudoEncodingStreamingModeAVCVAAPI,
             pseudoEncodingStreamingModeAVCVAAPI,
@@ -143,7 +140,7 @@ namespace rfb {
         static_assert(Encodings.size() == static_cast<size_t>(Encoder::unavailable) + 1, "Encodings array size must match Encoder enum count. ");
 
         static bool is_accelerated(Encoder encoder) {
-            return encoder != Encoder::h264_software && encoder != Encoder::h265_software && encoder != Encoder::av1_software;
+            return encoder != Encoder::h264_software && encoder != Encoder::av1_software;
         }
 
         static auto to_string(Encoder encoder) {
@@ -183,7 +180,6 @@ namespace rfb {
                 case Encoder::h265_vaapi: // h265
                 case Encoder::h265_ffmpeg_vaapi:
                 case Encoder::h265_nvenc:
-                case Encoder::h265_software:
                     return kasmVideoH265;
                 case Encoder::h264_vaapi:
                 case Encoder::h264_ffmpeg_vaapi:
@@ -205,7 +201,6 @@ namespace rfb {
                 case Encoder::h265_vaapi: // h265
                 case Encoder::h265_ffmpeg_vaapi:
                 case Encoder::h265_nvenc:
-                case Encoder::h265_software:
                     return pseudoEncodingStreamingModeHEVC;
                 case Encoder::h264_vaapi:
                 case Encoder::h264_ffmpeg_vaapi:
